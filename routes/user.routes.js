8db5e6c4a821648e717f11eb86bcb91dd5b54c49
CorentinDnt <donatocorentin2@gmail.com>
2021-08-21 17:00:36 +0200

    backend 1.0

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
+const uploadController = require("../controllers/upload.controller");
+const multer = require("multer");
+const upload = multer();
 
 //Authentification routes
 router.post("/register", authController.signUp);
@@ -16,5 +19,8 @@ router.delete("/:id", userController.delete);
 //Follow routes
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
+
+//Upload routes
+router.post("/upload", upload.single("file"), uploadController.uploadProfil);
 
 module.exports = router;
